856690e58112162e00ea20c4e4f434e862b02a27
James <jamesezhang@tencent.com>
2023-02-01 20:30:29 +0800

    feature: 一些格式调整

--- src/hoverI18n.js
+++ src/hoverI18n.js
@@ -1,9 +1,16 @@
-const { registerHoverProvider } = require('./utils/vs');
-const { provideHover } = require('./lib/provideHover');
+const { registerHoverProvider } = require("./utils/vs");
+const { provideHover } = require("./lib/provideHover");
 module.exports = (context) => {
-	context.subscriptions.push(
-		registerHoverProvider([ { scheme: 'file', language: 'vue' }, { scheme: 'file', language: 'javascript' } ], {
-			provideHover
-		})
-	);
+  const hoverSelectorList = [
+    { scheme: "file", language: "vue" },
+    { scheme: "file", language: "javascript" },
+    { scheme: "file", language: "javascriptreact" },
+    { scheme: "file", language: "typescript" },
+    { scheme: "file", language: "typescriptreact" },
+  ];
+  context.subscriptions.push(
+    registerHoverProvider(hoverSelectorList, {
+      provideHover,
+    })
+  );
 };
